a195a840d787341a5970f092e07b421f9e9ceb1c
brijesh <oddsworld0121@gmail.com>
2024-05-26 17:52:10 +0530

    install react js and tailwind css and create the first template

---
+++ mern-blog/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <div className={"text-3xl text-violet-700 font-black mx-auto px-20 py-10 flex justify-center items-center"}>
+            MERN BLOG
+        </div>
+    )
+}
+
+export default App
