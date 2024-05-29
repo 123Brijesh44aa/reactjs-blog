45980fc523db233b7ad16351fb3859183543fc09
brijesh <oddsworld0121@gmail.com>
2024-05-29 12:26:51 +0530

    mongo db connected

--- mern-blog-api/index.js
+++ mern-blog-api/index.js
@@ -1,7 +1,31 @@
-import express from "express";
+import express from 'express'
+import mongoose from 'mongoose'
+import { configDotenv } from 'dotenv'
 
-const app = express();
+configDotenv()
 
-app.listen(3009, () => {
-	console.log("Server is running on port 3009");
-});
+const mongo_db_password = process.env.MONGO_DB_PASSWORD
+const mongo_db_username = process.env.MONGO_DB_USERNAME
+const port = process.env.PORT | 3008
+let mongo_db_connection_url_string = process.env.MONGO_DB_CONNECTION_URL_STRING
+
+mongo_db_connection_url_string = mongo_db_connection_url_string.replace(
+  '<password>', mongo_db_password)
+mongo_db_connection_url_string = mongo_db_connection_url_string.replace(
+  '<username>', mongo_db_username)
+
+const app = express()
+
+mongoose.connect(mongo_db_connection_url_string).then(
+  () => {
+      return console.log('mongo db database connected successfully')
+  },
+).catch(
+  (error) => {
+      return console.log(error)
+  },
+)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
