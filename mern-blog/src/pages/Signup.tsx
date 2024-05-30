103319cc8aee2684a2dc01cda8772f089b129868
brijesh <oddsworld0121@gmail.com>
2024-05-30 20:58:23 +0530

    Add Functionality to the Sign-up page

--- mern-blog/src/pages/Signup.tsx
+++ mern-blog/src/pages/Signup.tsx
@@ -1,8 +1,72 @@
 import {images} from "@/utils/constants";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import Logo from "@/components/Logo";
+import React, {useState} from "react";
+
+interface SignupFormDataType {
+	username: string;
+	email: string;
+	password: string;
+}
+
+const InitialSignupFormData: SignupFormDataType = {
+	username: "",
+	email: "",
+	password: "",
+}
 
 const Signup = () => {
+
+	const [formData, setFormData] = useState<SignupFormDataType>(InitialSignupFormData);
+	const [error, setError] = useState<string | null>(null);
+	const [success, setSuccess] = useState<string | null>(null);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+
+	console.log(formData);
+
+	const handleSubmit = async (e: any) => {
+		e.preventDefault();
+		setError(null);
+		if (!formData.username || !formData.email || !formData.password) {
+			return setError("Please fill out all fields.");
+		}
+		if (formData.password.length < 8) {
+			return setError("Password must be at least 6 characters long.");
+		}
+		// check if email is valid
+		// regex for email validation
+		if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(formData.email)) {
+			return setError("Invalid email address.");
+		}
+
+		try {
+			setLoading(true);
+			setError(null);
+			const res = await fetch("http://localhost:3009/api/auth/signup", {
+				method: "POST",
+				headers: {"Content-Type": "application/json"},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success) {
+				setSuccess(data.message);
+				setLoading(false);
+				setError(null);
+				navigate("/sign-in");
+			}
+			if (!data.success) {
+				setError(data.message);
+				setLoading(false);
+				setSuccess(null);
+			}
+			console.log(data);
+		} catch (error: any) {
+			setError("Something went wrong. Please try again.");
+			setSuccess(null);
+			setLoading(false);
+		}
+	}
 
 	return (
 		<>
@@ -16,43 +80,117 @@ const Signup = () => {
 
 							<div className={"mb-10"}>
 								<Logo width={"400"} height={"200"}/>
-								<h1 className={"text-2xl font-bold text-gray-800 py-3"}>Sign Up with your email and password or with Google</h1>
-								<p>Dive into a world where curiosity meets creativity. Sign up to join our community and stay updated with the latest posts. It's all about sharing stories and insights, one post at a time.</p>
+								<h1 className={"text-2xl font-bold text-gray-800 py-3"}>Sign Up with your email and
+									password or with Google</h1>
+								<p>Dive into a world where curiosity meets creativity. Sign up to join our community and
+									stay updated with the latest posts. It's all about sharing stories and insights, one
+									post at a time.</p>
 							</div>
 
-							<form className={"flex flex-col justify-between items-start gap-6"}>
+							<form
+								onSubmit={handleSubmit}
+								className={"flex flex-col justify-between items-start gap-6"}>
 
-								<div className={"w-full"}>
-									<label className={"text-gray-800 font-medium"}>
+								<div
+									className={"w-full"}>
+									<label
+										className={"text-gray-800 font-medium"}>
 										Your username
 									</label>
-									<input id={"username"} name={"username"} type={"text"} autoComplete={"username"}
+									<input id={"username"}
+									       name={"username"}
+									       type={"text"}
+									       autoComplete={"username"}
 									       placeholder={"Username"}
-									       className={"w-full mt-2 px-4 py-3 outline-none ring-1 ring-gray-300 focus:ring-1 focus:ring-btn_bg rounded-md"}/>
+									       onChange={(event) => {
+										       return (
+											       setFormData(
+												       {
+													       ...formData, username: event.target.value.trim(),
+												       }
+											       )
+										       );
+									       }}
+									       className={"w-full mt-2 px-4 py-3 outline-none ring-1 ring-gray-300 focus:ring-1 focus:ring-btn_bg rounded-md"}
+									/>
+
 								</div>
 
-								<div className={"w-full"}>
+								<div
+									className={"w-full"}>
 									<label className={"text-gray-800 font-medium"}>
 										Your email
 									</label>
-									<input id={"email"} name={"email"} type={"email"} autoComplete={"email"}
-									       placeholder={"example@gmail.com"}
-									       className={"w-full mt-2 px-4 py-3 outline-none ring-1 ring-gray-300 focus:ring-1 focus:ring-btn_bg rounded-md"}/>
+									<input
+										id={"email"}
+										name={"email"}
+										type={"email"}
+										autoComplete={"email"}
+										placeholder={"example@gmail.com"}
+										onChange={(event) => {
+											return (
+												setFormData(
+													{
+														...formData, email: event.target.value.trim(),
+													}
+												)
+											);
+										}}
+										className={"w-full mt-2 px-4 py-3 outline-none ring-1 ring-gray-300 focus:ring-1 focus:ring-btn_bg rounded-md"}
+									/>
+
 								</div>
 
-								<div className={"w-full"}>
+								<div
+									className={"w-full"}>
 									<label className={"text-gray-800 font-medium"}>
 										Your password
 									</label>
-									<input id={"password"} name={"password"} type={"password"}
-									       autoComplete={"new-password"} placeholder={"Password"}
-									       className={"w-full mt-2 px-4 py-3 outline-none ring-1 ring-gray-300 focus:ring-1 focus:ring-btn_bg rounded-md"}/>
+									<input
+										id={"password"}
+										name={"password"}
+										type={"password"}
+										autoComplete={"new-password"}
+										placeholder={"Password"}
+										onChange={(event) => {
+											return (
+												setFormData(
+													{
+														...formData, password: event.target.value.trim(),
+													}
+												)
+											);
+										}}
+										className={"w-full mt-2 px-4 py-3 outline-none ring-1 ring-gray-300 focus:ring-1 focus:ring-btn_bg rounded-md"}
+									/>
 								</div>
 
-								<div className={"w-full"}>
-									<button type={"submit"}
-									        className={"w-full mt-2 px-4 py-2 bg-btn_bg font-medium text-white rounded-md"}>
-										Sign Up
+								{
+									error && (
+										<div className={"w-full px-4 py-2 bg-red-100 text-red-600 rounded-md"}>
+											{error}
+										</div>
+									)
+								}
+
+								{
+									success && (
+										<div className={"w-full px-4 py-2 bg-green-100 text-green-600 rounded-md"}>
+											{success}
+										</div>
+									)
+								}
+
+								<div
+									className={"w-full"}>
+									<button
+										type={"submit"}
+										disabled={loading}
+										className={"w-full mt-2 px-4 py-2 bg-btn_bg font-medium text-white rounded-md"}
+									>
+										{
+											loading ? (<span className={"pl-3"}>Loading...</span>) : "Sign Up"
+										}
 									</button>
 								</div>
 
@@ -68,6 +206,7 @@ const Signup = () => {
 								</div>
 
 							</form>
+
 						</div>
 					</div>
 
@@ -79,10 +218,11 @@ const Signup = () => {
 								images.map((image, index) => {
 									return (
 										<div
-											className={`overflow-hidden rounded-xl ${index === 5 ? "col-span-3" : (index === 7 ? "col-span-2 row-span-2" : (index === 10 ? "row-span-3" : (index === 14 || index === images.length ? "col-span-2" : "")))}`}>
+											className={`overflow-hidden rounded-xl ${index === 5 ? "col-span-3" : (index === 7 ? "col-span-2 row-span-2" : (index === 10 ? "row-span-3" : (index === 14 || index === images.length ? "col-span-2" : "")))}`}
+											key={index}
+										>
 											<img
 												className={`w-full h-40 object-cover ${index === 7 || index === 10 ? "h-[100%]" : ""}`}
-												key={index}
 												src={image.url}
 												alt={image.url}
 											/>
